59db189f34e123405e6d15c81e121cda044c3182
SilverYaong <152715113+SilverYaong@users.noreply.github.com>
2025-01-31 10:08:10 +0900

    [D3] Title: 햄버거 다이어트, Time: 38 ms, Memory: 5,992 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.cpp
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.cpp
@@ -1,35 +1,22 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int max_score;
-int current_score;
-
-void subsets(const int score[], const int calorie[], const int limit, const int size)
+int solve(const vector<int> score, const vector<int> calorie, const int limit, const int size)
 {
-    int total = 1 << size;
+    vector<int> dp(limit + 1, 0);
 
-    for (int i = 0; i < total; i++)
+    for (int i = 0; i < size; i++)
     {
-        int calorie_sum = 0;
-        int score_sum = 0;
-        for (int j = 0; j < size; j++)
+        for (int j = limit; j >= calorie[i]; j--)
         {
-            if (i & (1 << j))
-            {
-                score_sum += score[j];
-                calorie_sum += calorie[j];
-            }
-        }
-        if (calorie_sum > limit)
-        {
-            continue;
-        }
-        else if (max_score < score_sum)
-        {
-            max_score = score_sum;
+            dp[j] = max(dp[j], dp[j - calorie[i]] + score[i]);
         }
     }
+
+    return *max_element(dp.begin(), dp.end());
 }
 
 int main()
@@ -42,16 +29,15 @@ int main()
 
     for (int test_case = 0; test_case < t; test_case++)
     {
-        max_score = 0;
         int ans, n, limit;
         cin >> n >> limit;
-        int score[n], calorie[n];
+        vector<int> score(n), calorie(n);
         for (int i = 0; i < n; i++)
         {
             cin >> score[i] >> calorie[i];
         }
 
-        subsets(score, calorie, limit, n);
-        cout << "#" << test_case + 1 << " " << max_score << endl;
+        ans = solve(score, calorie, limit, n);
+        cout << "#" << test_case + 1 << " " << ans << endl;
     }
 }
