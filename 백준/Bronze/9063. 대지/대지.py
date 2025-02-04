c69091417b3b5450f3d78e712fd884ae6d21687a
SilverYaong <152715113+SilverYaong@users.noreply.github.com>
2025-02-04 15:04:02 +0900

    [Bronze III] Title: 대지, Time: 1344 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.py
+++ 백준/Bronze/9063. 대지/대지.py
@@ -1,21 +1,19 @@
-n = int(input())
-arr = []
-for i in range(n):
-    arr.append(list(map(int, input().split())))
 max_x = -10000
 min_x = 10000
 max_y = -10000
 min_y = 10000
 
-for i in arr:
-    if max_x < i[0]:
-        max_x = i[0]
-    if min_x > i[0]:
-        min_x = i[0]
-    if max_y < i[1]:
-        max_y = i[1]
-    if min_y > i[1]:
-        min_y = i[1]
-        
+n = int(input())
+for i in range(n):
+    x, y = map(int, input().split())
+    if max_x < x:
+        max_x = x
+    if min_x > x:
+        min_x = x
+    if max_y < y:
+        max_y = y
+    if min_y > y:
+        min_y = y
+
 ans = (max_x - min_x) * (max_y - min_y)
 print(ans)    
