0cc6cc8dab0fb9a4d2fca9353c94df883b78b1f3
SilverYaong <152715113+SilverYaong@users.noreply.github.com>
2025-02-11 15:02:19 +0900

    [Bronze II] Title: OX퀴즈, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
@@ -0,0 +1,12 @@
+n = int(input())
+for _ in range(n):
+    s = input()
+    cnt = 0
+    ans = 0
+    for i in s:
+        if i == 'O':
+            cnt += 1
+            ans += cnt
+        else:
+            cnt = 0
+    print(ans)
