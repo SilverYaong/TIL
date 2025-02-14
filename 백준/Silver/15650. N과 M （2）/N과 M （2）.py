95956ed07b2312c6225bd623e8ca822ae7272e9f
SilverYaong <152715113+SilverYaong@users.noreply.github.com>
2025-02-14 16:18:13 +0900

    [Silver III] Title: N과 M (2), Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,10 @@
+def sol(start, arr):
+    if len(arr) == m:
+        print(*arr)
+        return
+    for i in range(start, n + 1):
+        sol(i + 1, arr + [i])
+
+n, m = map(int, input().split())
+
+sol(1, [])
