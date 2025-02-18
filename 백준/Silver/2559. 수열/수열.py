a4bfd2598417873a6e62088bfa941666a20e0644
SilverYaong <152715113+SilverYaong@users.noreply.github.com>
2025-02-18 12:20:02 +0900

    [Silver III] Title: 수열, Time: 80 ms, Memory: 40120 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.py
@@ -0,0 +1,8 @@
+n, k = map(int, input().split())
+arr = [*map(int, input().split())]
+sum_arr = [0] * (n - k + 1)
+ans = 0
+sum_arr[0] = sum(arr[:k])
+for i in range(1, n - k + 1):
+    sum_arr[i] = sum_arr[i - 1] - arr[i - 1] + arr[i + k - 1]
+print(max(sum_arr))
