3a0ad4f5f7d819dbc0407c3f7769b1656b420489
SilverYaong <152715113+SilverYaong@users.noreply.github.com>
2025-04-01 15:31:22 +0900

    [Silver IV] Title: 수 찾기, Time: 64 ms, Memory: 1388 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,6 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <set>
+#include <algorithm>
 
 using namespace std;
 
@@ -8,18 +8,18 @@ int main() {
 	//freopen("input.txt", "r", stdin);
 	int N;
 	scanf("%d", &N);
-	multiset<int> A;
+	int A[100000];
 	for (register int i = 0; i < N; i++) {
-		int a;
-		scanf("%d", &a);
-		A.insert(a);
+		scanf("%d", &A[i]);
 	}
 	int M;
 	scanf("%d", &M);
+	sort(A, A + N);
 	for (register int i = 0; i < M; i++) {
 		int target;
 		scanf("%d", &target);
-		if (A.find(target) != A.end()) {
+		bool res = binary_search(A, A + N, target);
+		if (res) {
 			printf("1\n");
 		}
 		else {
