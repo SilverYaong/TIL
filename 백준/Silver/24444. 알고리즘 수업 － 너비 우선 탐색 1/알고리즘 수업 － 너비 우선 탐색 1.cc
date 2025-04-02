fd096ae8413919275f31e5c94f056beaed5fcbe4
SilverYaong <152715113+SilverYaong@users.noreply.github.com>
2025-04-02 17:23:29 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 108 ms, Memory: 9312 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int main() {
+	//freopen("input.txt", "r", stdin);
+	int N, M, R;
+	scanf("%d %d %d", &N, &M, &R);
+	vector<priority_queue<int, vector<int>, greater<int>>> graph(N + 1);
+	for (register int i = 0; i < M; i++) {
+		int u, v;
+		scanf("%d %d", &u, &v);
+		graph[u].push(v);
+		graph[v].push(u);
+	}
+
+	vector<bool> vis(N + 1, 0);
+	vector<int> order(N + 1, 0);
+	queue<int> q;
+	q.push(R);
+	vis[R] = 1;
+	int idx = 1;
+
+	while (!q.empty()) {
+		int u = q.front(); q.pop();
+		order[u] = idx++;
+		while (!graph[u].empty()) {
+			int v = graph[u].top(); graph[u].pop();
+			if (!vis[v]) {
+				vis[v] = 1;
+				q.push(v);
+			}
+		}
+	}
+	for (register int i = 1; i <= N; i++) {
+		printf("%d\n", order[i]);
+	}
+}
