44f41662d6e6a486629dce74c2ea41d516188f8d
SilverYaong <152715113+SilverYaong@users.noreply.github.com>
2026-05-17 19:47:29 +0900

    [level 2] Title: 숫자의 표현, Time: 0.03 ms, Memory: 4.05 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    for (int i = 1; i <= n; ++i)
+    {
+        int sum = 0;
+        int cnt = i;
+        while(sum < n)
+        {
+            sum += cnt++;
+            if (sum == n) answer++;
+        }
+    }
+    
+    
+    return answer;
+}
